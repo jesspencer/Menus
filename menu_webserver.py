3d8638a170f29250b35dfc4db826f74c4b3844f6
Jessica.E.Spencer <jessicaspencer12@gmail.com>
2019-03-01 23:49:15 -0500

    Updated file with get_Post

--- menu_webserver.py
+++ menu_webserver.py
@@ -1,14 +1,13 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import cgi
 
-
 #import CRUD operations from database_setup.py
 from database_setup import Base, Restaurant, MenuItem
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 #code that creates the database
 engine = create_engine('sqlite:///restaurantmenu.db')
-Base.metadata.create_all(engine)
+Base.metadata.bind= engine
 #code that creates the session is imported from menuAdds.py
 DBSession = sessionmaker(bind = engine)
 session = DBSession()
@@ -23,15 +22,15 @@ class webServerHandler(BaseHTTPRequestHandler):
                 self.end_headers()
                 output = ""
                 output += "<html><body>"
-                output += "<h1>Make a New Restaurant<h1>"
-                output += "form method = 'POST' enctype = 'multipart/form-data' action = 'restaurants/new'>"
-                output += "<input name = 'newRestaurnatName' type = 'text' placeholder = 'New Restaurant Name'>"
+                output += "<h1>Make a New Restaurant</h1>"
+                output += "form method = 'POST' enctype='multipart/form-data' action = '/restaurants/new'>"
+                output += "<input name = 'newRestaurantName' type = 'text' placeholder = 'New Restaurant Name'>"
                 output += "<input type = 'submit' value = 'Create'>"
                 output += "</form></body></html>"
                 self.wfile.write(output)
                 return
 
-            if self.path.endswith("restaurants"):
+            if self.path.endswith("/restaurants"):
                 restaurants = session.query(Restaurant).all()
                 output = ""
             #objective step3 - Cretate a Link to create a new menuItem
@@ -53,51 +52,25 @@ class webServerHandler(BaseHTTPRequestHandler):
                 self.wfile.write(output)
                 return
         except IOError:
-
-
-                restaurants = session.query(Restaurant).all()
-
-                output = ""
-                #link so user can create a new restaurant
-                self.send_response(200)
-                self.send_header('Content-type', 'text/html')
-                self.end_headers()
-
-                output = ""
-                output += '<html><body>'
-                #add for statement
-                for restaurant in restaurants:
-                    output += restaurant.name
-                    output += "</br>" # br means break the line
-                    output += "<a href = '#' > Edit </a>"
-                    output += "</br>"
-                    output += "<a href = '#' > Delete </a>"
-                    output += "</br></br></br>"
-
-                output += "</body></html>"
-                self.wfile.write(output)
-                return
-
-        except IOError:
-            self.send_error(404,"File not Found %s" % self.path)
-#objective 3 is to call a get_post method
+            self.send_error(404, 'File Not Found: %s' % self.path)
+    #Objective 3 Seetp3 --Make Post method
     def do_POST(self):
         try:
             if self.path.endswith("restaurants/new"):
                 ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
-            if ctype == 'multipart/form-data':
-                fields = cgi.parse_multipart(self.rfile, pdict)
-                messagecontent = fields.get('newRestaurantName')
-                #create new restaurant object
+                if ctype == 'multipart/form-data':
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                    messagecontent = fields.get('newRestaurantName')
+                    #create new restaurant object
 
-                newRestaurant = Restaurant(name = messagecontent[0])
-                session.add(newRestaurant)
-                session.commit()
+                    newRestaurant = Restaurant(name = messagecontent[0])
+                    session.add(newRestaurant)
+                    session.commit()
 
-                self.send_response(301)
-                self.send_header('Content-type', 'text/html')
-                self.send_header('Location', 'restaurants')
-                self.end_headers()
+                    self.send_response(301)
+                    self.send_header('Content-type', 'text/html')
+                    self.send_header('Location', '/restaurants')
+                    self.end_headers()
         except:
             pass
 
@@ -110,8 +83,6 @@ def main():
     except KeyboardInterrupt:
         print "^C entered, stopping sever ..."
         server.socket.close()
-
-
 
 
 if __name__ =='__main__':
