ad4f6835ad7d8939e464baaf6dde2c166763f7f4
Jessica.E.Spencer <jessicaspencer12@gmail.com>
2019-03-12 15:12:35 -0400

    fix:Added a space in line 67 between the '%' and the variable restaurantIDPath to allow the form method to display when the delete button is clicked

--- menu_webserver.py
+++ menu_webserver.py
@@ -64,7 +64,7 @@ class webServerHandler(BaseHTTPRequestHandler):
                     output = ""
                     output += "<html><body>"
                     output += "<h1> Are you sure you want to delete this %s ?" % myRestaurantQuery.name
-                    output += "<form method = 'POST' enctype = 'multipart/form-data' action = '/restaurants/%s/delete'>" %restaurantIDPath
+                    output += "<form method = 'POST' enctype = 'multipart/form-data' action = '/restaurants/%s/delete'>" % restaurantIDPath
                     output += "</form>"
                     output += "</body></html>"
                     self.wfile.write(output)
