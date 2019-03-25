b8d916d77dd7a9b414d0195078db3cefadc19a50
Jessica.E.Spencer <jessicaspencer12@gmail.com>
2019-03-25 10:05:00 -0400

    Added sql database functionality to Flask app. Listing the price, menu, item decription

--- project.py
+++ project.py
@@ -1,22 +1,23 @@
-from flask import Flask
 
+from flask import Flask
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
-
 app = Flask(__name__)
-#decorator in python
+
+
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.bind = engine
-DBSession = sessionmaker(bind = engine)
+
+DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-@app.route('/restaurants/<int:restaurant_id>/')
-def restaurantMenu(restaurant_id):
-    DBSession = sessionmaker(bind = engine)
-    session = DBSession()
-    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
-    items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
+
+@app.route('/')
+@app.route('/hello')
+def HelloWorld():
+    restaurant = session.query(Restaurant).first()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
     output = ''
     for i in items:
         output += i.name
@@ -30,5 +31,4 @@ def restaurantMenu(restaurant_id):
 
 if __name__ == '__main__':
     app.debug = True
-    #tells the webserver on vagrant to listen on all public ports
-    app.run(host = '0.0.0.0', port = 5000)
+    app.run(host= '0.0.0.0', port= 5000)
