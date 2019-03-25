7642cb5792d7cb502d1fc6400a8533443af1d190
Jessica.E.Spencer <jessicaspencer12@gmail.com>
2019-03-25 19:23:51 -0400

    Adding flash messaging when item is create, edited, or deleted

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -26,10 +26,10 @@ def restaurantMenu(restaurant_id):
 @app.route('/restaurant/<int:restaurant_id>/new/', methods=['GET', 'POST'])
 def newMenuItem(restaurant_id):
     if request.method == 'POST':
-        newItem = MenuItem(
-            name=request.form['name'], restaurant_id=restaurant_id)
+        newItem = MenuItem(name=request.form['name'], restaurant_id=restaurant_id)
         session.add(newItem)
         session.commit()
+        flash("new menu item created!")
         return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
     else:
         return render_template('newmenuitem.html', restaurant_id=restaurant_id)
@@ -45,6 +45,7 @@ def editMenuItem(restaurant_id, menu_id):
             editedItem.name = request.form['name']
         session.add(editedItem)
         session.commit()
+        flash("item was edited!")
         return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
     else:
         return render_template('editmenuitem.html', restaurant_id=restaurant_id, menu_id = menu_id, i = editedItem)
@@ -58,10 +59,12 @@ def deleteMenuItem(restaurant_id, menu_id):
     if request.method =='POST':
         session.delete(itemToDelete)
         session.commit()
+        flash("item has been deleted")
         return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
     else:
         return render_template('deleteMenuItem.html', item= itemToDelete)
 
 if __name__ == '__main__':
     app.debug = True
+    app.secret_key= 'super_secret_key'
     app.run(host='0.0.0.0', port=5000)
